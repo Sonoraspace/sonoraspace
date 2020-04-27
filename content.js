36f247a4d7ce63a2fe308bf9e06b0a495f87bc18
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-04-27 19:42:39 +1000

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var listingFieldDescription = {
     "Price": "Some description goes here",
     "Availability calendar*": "Add some description text",
     "Genre*": "Lorem Ipsum 1",
-    "Musician Bio*": 'To  open a iframe <a href="https://www.lipsum.com/" data-featherlight="iframe" data-featherlight-iframe-height="640" data-featherlight-iframe-width="480" >click me</a>',
+    "Musician Bio*": 'To  open a iframe <a href="https://www.lipsum.com/" data-featherlight="iframe" data-featherlight-iframe-height="940" data-featherlight-iframe-width="480" >click me</a>',
     "Language*": 'To  open a youtube video <a href="https://www.youtube.com/embed/9t3kU77C7H4" data-featherlight="iframe" data-featherlight-iframe-height="480" data-featherlight-iframe-width="640" >click me</a>'
 
 };
