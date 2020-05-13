a960cd65cc3f2c3253f50c0663d5975002e0f29c
Sonoraspace <64307127+Sonoraspace@users.noreply.github.com>
2020-05-13 11:51:53 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var listingFieldDescription = {
     "Listing title*": "please enter your name here.",
     "Price": 'Please set a price for your session. If someone books an hour (2x30 min), the price will be doubled automatically. As currency we use US-Dollar. If you want to know how much, for example, $20 is in your currency, you can use <a href="https://transferwise.com/us/currency-converter" data-featherlight="iframe" data-featherlight-iframe-height="450" data-featherlight-iframe-width="450" >this converter by Transwise here.</a>',
-    "Instruments & skills*": "Please choose at least one instrument or skill that you can master.".
+    "Instruments & skills*": "Please choose at least one instrument or skill that you can master.",
     "Genre*": "Please select at least one genre.",
     "Language*": "Please select at least one language that you can speak.",
     "Musician Bio*": 'To  open a iframe <a href="https://www.lipsum.com/" data-featherlight="iframe" data-featherlight-iframe-height="940" data-featherlight-iframe-width="480" >click me</a>',
