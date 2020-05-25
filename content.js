143cc2995216c552745115443359942fc8ea2640
Sonoraspace <64307127+Sonoraspace@users.noreply.github.com>
2020-05-25 16:13:45 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 var listingFieldDescription = {
     "Listing title*": "Please enter your name here.",
-    "Detailed description*": "Please write down a few words about yourself & what can be expected from your sessions.",
+    "Detailed description": "Please write down a few words about yourself & what can be expected from your sessions.",
     "Price": 'Please set a price for your session. If someone books an hour (2x30 min), the price will be doubled automatically. As currency we use US-Dollar. If you want to know how much, for example, $20 is in your currency, you can use <a href="https://transferwise.com/us/currency-converter" data-featherlight="iframe" data-featherlight-iframe-height="450" data-featherlight-iframe-width="100%" >this converter by Transwise here.</a>',
     "Instruments & skills*": "Please select at least one instrument or skill that you can master.",
-    "Avaibility*": "Please tell us your avaibility, e.g. Monday - Friday: 1pm - 4pm & Saturday: 10am - 1pm.",
+    "Availability*": "Please tell us your avaibility, e.g. Monday - Friday: 1pm - 4pm & Saturday: 10am - 1pm.",
     "Timezone*": "Please select your timezone.",
     "Genre*": "Please select at least one genre.",
     "Language*": "Please select at least one language that you can speak.",
